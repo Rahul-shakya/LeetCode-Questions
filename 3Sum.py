940488d222e179a08812c6a0733ed6c1e99d4592
Rahul Shakya <rahul.shakya3@gmail.com>
2021-06-05 09:53:22 +0530

    Slight changes made to 3Sum. Added condition checks to skip cases with duplicate values. Execution time reduced by half

--- 3Sum.py
+++ 3Sum.py
@@ -4,12 +4,18 @@ class Solution:
         res = []
         nums.sort()
         for i in range(len(nums)-2):
+            if(i > 0 and nums[i] == nums[i-1]):
+                continue
             currA = nums[i]
             left = i+1
             right = len(nums)-1
             while(left < right):
                 if((currA + nums[left] + nums[right]) == 0 and [currA,nums[left],nums[right]] not in res):
                     res.append([currA,nums[left],nums[right]])
+                    while left < right and nums[left] == nums[left+1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right-1]:
+                        right -= 1
                     left+=1
                     right-=1
                 elif((currA + nums[left] + nums[right]) < 0):
