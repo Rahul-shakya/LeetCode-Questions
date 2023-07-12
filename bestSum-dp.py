73790dfe19f024942af4e088cb2126fdcccad757
Rahul Shakya <rahul.shakya3@gmail.com>
2023-07-11 20:17:30 -0700

    Create bestSum-dp.py
    best Sum from freeCodeCamp

---
+++ bestSum-dp.py
@@ -0,0 +1,44 @@
+
+def bestSumNoDP(target, arr):
+    if target == 0:
+        return []
+    if target < 0:
+        return None
+    ans = None
+    for num in arr:
+        remainder = target - num
+        result = bestSumNoDP(remainder, arr)
+        if result is not None:
+            result.append(num)
+            if(ans == None or (len(result) < len(ans))):
+                ans = result
+    return ans
+
+
+def bestSumDP(target, arr, memo = None):
+    if(memo == None):
+        memo = {}
+    if(target in memo):
+        return memo[target]
+    if target == 0:
+        return []
+    if target < 0:
+        return None
+    ans = None
+    for num in arr:
+        remainder = target - num
+        result = bestSumDP(remainder, arr, memo)
+        if(result is not None):
+            combi = result + [num]
+            if(ans == None or (len(combi) < len(ans))):
+                ans = combi
+    memo[target] = ans
+    return ans
+
+
+
+testCases = [(7, [2, 4, 3]), (7, [5, 3, 4, 7]), (7, [2, 4]), (8, [2, 3, 5]),(100,[1, 2, 5, 25])]
+
+for test in testCases:
+    targetSum, numbers = test
+    print(f'bestSumDP({targetSum}, {numbers})={bestSumDP(targetSum, numbers)}')
