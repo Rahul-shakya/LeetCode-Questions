d6f3b22d3fd880d54b3147a284a0280f0923e8cd
Rahul Shakya <rahul.shakya3@gmail.com>
2023-08-21 11:24:38 -0500

    Added dailytemp, nextGreaterEle, binSearch

---
+++ search.py
@@ -0,0 +1,20 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        high = len(nums) - 1
+        low = 0
+        while(low <= high):
+            mid = low + (high-low)//2
+            if nums[mid] == target:
+                return mid
+            if nums[mid] < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+        return -1
+    
+
+        for currList in matrix:
+            if(search(self, currList, target)):
+                return True
+        return False
+    
